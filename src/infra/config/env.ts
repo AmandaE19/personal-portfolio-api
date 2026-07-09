56681e0be9d01d483c980d167c03ffb2e7b2f5d5
AmandaE19 <amanda.emidio.557@gmail.com>
2026-07-09 18:28:24 -0400

    Altera port em .env

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const env = {
-  PORT: Number(process.env.PORT) || 3000,
+  PORT: Number(process.env.PORT) || 5000,
   RESEND_API_KEY: process.env.RESEND_API_KEY!,
   EMAIL_TO: process.env.EMAIL_TO!,
   URL_FRONTEND: process.env.URL_FRONTEND!,
